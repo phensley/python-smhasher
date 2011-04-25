6d40c2035d0e4153023fd4452b84ca90e084bc96
Patrick Hensley <spaceboy@indirect.com>
2011-04-25 16:42:45 -0400

    Released smhasher 0.135.2.
     * Added 128-bit hash functions, updated tests.

--- smhasher.cpp
+++ smhasher.cpp
@@ -12,8 +12,11 @@
 #include "MurmurHash3.h"
 
 
+typedef void (*murmur3_func)(const void *, int, uint32_t, void *);
+
+
 static PyObject *
-py_murmur3_x86_64(PyObject *self, PyObject *args)
+_py_murmur3_64(PyObject *self, PyObject *args, murmur3_func hashfunc)
 {
     const char *key;
     Py_ssize_t len;
@@ -23,33 +26,90 @@ py_murmur3_x86_64(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "s#|I", &key, &len, &seed)) {
         return NULL;
     }
-    MurmurHash3_x86_128((void *)key, len, seed, &out);
+    hashfunc((void *)key, len, seed, &out);
     // return the first 64 bits
     return PyLong_FromUnsignedLongLong(out[0]);
+}
+
+
+static PyObject *
+py_murmur3_x86_64(PyObject *self, PyObject *args)
+{
+    return _py_murmur3_64(self, args, MurmurHash3_x86_128);
 }
 
 
 static PyObject *
 py_murmur3_x64_64(PyObject *self, PyObject *args)
 {
+    return _py_murmur3_64(self, args, MurmurHash3_x64_128);
+}
+
+
+static PyObject *
+_py_murmur3_128(PyObject *self, PyObject *args, murmur3_func hashfunc)
+{
     const char *key;
     Py_ssize_t len;
     uint32_t seed = 0;
     uint64_t out[2];
+    PyNumberMethods *long_methods = PyLong_Type.tp_as_number;
+    PyObject * denom = 0;
+    PyObject *hi = 0;
+    PyObject *lo = 0;
+    PyObject *tmp = 0;
+    PyObject *result = 0;
 
     if (!PyArg_ParseTuple(args, "s#|I", &key, &len, &seed)) {
         return NULL;
     }
-    MurmurHash3_x64_128((void *)key, len, seed, &out);
-    // return the first 64 bits
-    return PyLong_FromUnsignedLongLong(out[0]);
+
+    // get the hi and lo parts of the 128-bit hash value
+    hashfunc((void *)key, len, seed, &out);
+//    MurmurHash3_x64_128((void *)key, len, seed, &out);
+    hi = PyLong_FromUnsignedLongLong(out[0]);
+    lo = PyLong_FromUnsignedLongLong(out[1]);
+
+    // result = (hi << 64) + lo
+    denom = PyLong_FromLong(64);
+    tmp = long_methods->nb_lshift(hi, denom);
+    result = long_methods->nb_add(tmp, lo);
+
+    Py_DECREF(denom);
+    Py_DECREF(tmp);
+    Py_DECREF(hi);
+    Py_DECREF(lo);
+    return result;
 }
+
+
+static PyObject *
+py_murmur3_x86_128(PyObject *self, PyObject *args)
+{
+    return _py_murmur3_128(self, args, MurmurHash3_x86_128);
+}
+
+
+static PyObject *
+py_murmur3_x64_128(PyObject *self, PyObject *args)
+{
+    return _py_murmur3_128(self, args, MurmurHash3_x64_128);
+}
+
 
 PyDoc_STRVAR(module_doc, "Python wrapper for the SMHasher routines.");
 
 static PyMethodDef smhasher_methods[] = {
-    {"murmur3_x86_64", py_murmur3_x86_64, METH_VARARGS, "Make x86 64-bit hash"},
-    {"murmur3_x64_64", py_murmur3_x64_64, METH_VARARGS, "Make x64 64-bit hash"},
+    {"murmur3_x86_64", py_murmur3_x86_64, METH_VARARGS,
+        "Make an x86 murmur3 64-bit hash value"},
+    {"murmur3_x64_64", py_murmur3_x64_64, METH_VARARGS,
+        "Make an x64 murmur3 64-bit hash value"},
+
+    {"murmur3_x86_128", py_murmur3_x86_128, METH_VARARGS,
+        "Make an x86 murmur3 128-bit hash value"},
+    {"murmur3_x64_128", py_murmur3_x64_128, METH_VARARGS,
+        "Make an x64 murmur3 128-bit hash value"},
+
     {NULL, NULL, 0, NULL}
 };
 
--- test_3x.py
+++ test_3x.py
@@ -9,6 +9,10 @@ print(smhasher.murmur3_x86_64('samplebias'))
 # with seed value
 print(smhasher.murmur3_x86_64('samplebias', 123))
 
+# 128-bit hashes
+print('128-bit x64: %s' % smhasher.murmur3_x64_128('samplebias'))
+print('128-bit x86: %s' % smhasher.murmur3_x86_128('samplebias'))
+
 # timing comparison with str __hash__
 t = timeit.Timer("smhasher.murmur3_x86_64('hello')", "import smhasher")
 print('smhasher.murmur3:', t.timeit())
