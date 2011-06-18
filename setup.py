0eede7241b201800b653f9746d3d20cccf46360c
Patrick Hensley <spaceboy@indirect.com>
2011-06-18 13:56:43 -0400

    Packaged for PyPi.
     * Added PyPi classifiers and package MANIFEST.in.
     * Symlinked README.txt.
     * Bumped version to 0.136.2.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup, Extension
 
 # version number is x.[upstream svn revision].release until upstream
 # creates a formal version number.
-VERSION = '0.136.1'
+VERSION = '0.136.2'
 
 # avoid building universal binary (ppc) on osx non-ppc platforms
 if sys.platform == 'darwin':
@@ -23,10 +23,38 @@ smhasher_ext = Extension('smhasher',
     define_macros=[('MODULE_VERSION', '"%s"' % VERSION)])
 
 
-setup(
-    name='smhasher',
-    version=VERSION,
-    description='Python extension for smhasher hash functions',
-    ext_modules=[smhasher_ext]
-    )
+if __name__ == '__main__':
+    # load README.txt for the long description
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(cwd, 'README.txt')
+    readme = open(path, 'rb').read()
+
+    # call setup
+    setup(
+        name = 'smhasher',
+        version = VERSION,
+        description = 'Python extension for smhasher hash functions',
+        author = 'Patrick Hensley',
+        author_email = 'spaceboy@indirect.com',
+        keywords = ['hash', 'hashing', 'smhasher'],
+        url = "http://github.com/phensley/python-smhasher",
+        ext_modules = [smhasher_ext],
+        classifiers = [
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX :: Linux",
+            "Operating System :: Unix",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.4",
+            "Programming Language :: Python :: 2.5",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.1",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            ],
+        long_description = readme,
+        )
 
